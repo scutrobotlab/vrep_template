1f285cb8f10373aa72c95b8e95e2a75766093d90
clang <clangwu@163.com>
2021-10-10 13:48:42 +0800

    [add]some funny functions

--- core_test/car.cpp
+++ core_test/car.cpp
@@ -2,8 +2,14 @@
 void mycar::car_spin()
 {
         static uint8_t dir = 1;
-        mycar_data.joint_data.tar_pos[servo] = 15*3.14/180 * dir;
-        dir = !dir;
+        static uint8_t cnt = 0;
+        mycar_data.joint_data.tar_pos[servo] = 25*3.14/180 * dir;
+        if(cnt>= 10)
+        {
+                cnt = 0;
+                dir = !dir;
+        }
+        cnt++;
 
         for (size_t i = 0; i < 4; i++)
                 mycar_data.joint_data.tar_vel[i] = -10;
